445faab7af076f888a9e6b56949bf986eaf938c8
Fernandoramos24 <87905334+Fernandoramos24@users.noreply.github.com>
2021-09-10 23:11:53 -0600

    ejercicio 2 semana 6

---
+++ ejercicio 2.py
@@ -0,0 +1,11 @@
+Temperatura = float(input("Inserte temperatura: "))
+
+
+if Temperatura<=10 :
+    print("Frio")
+elif Temperatura>10 or Temperatura<=20:
+    print("Nublado")
+elif Temperatura>20 or Temperatura<=30:
+    print("Caluroso")
+elif Temperatura>30:
+    print("Tropical")
